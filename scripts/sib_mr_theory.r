af61b7c37b6c95f13903b0c3a8e9bd2b9391b27b
Gibran Hemani <explodecomputer@gmail.com>
2017-02-23 20:41:50 +0000

    updated

---
+++ scripts/sib_mr_theory.r
@@ -0,0 +1,34 @@
+
+
+n <- 1000
+q <- sqrt(0.2)
+f <- sqrt(0.2)
+r <- sqrt(1-q^2-f^2)
+alpha <- 0.01
+
+vard2 <- 8 * r^2 * (1 - f^2) + (7/2) * q^4
+vars2 <- 8 * (1 + f^2) * (1 + q^2 + f^2) + (7/2) * q^4
+(tval <- 0.5 * (n - 2) * q^4 / vard2 - 0.5 * (n - 2) * q^4 / vars2) + 0.5
+
+pt(tval, n, lower.tail=FALSE)
+thr <- qt(1-alpha, n)
+1 - pt(thr, n, ncp=tval)
+
+
+
+
+F <- rnorm(n, sd=f)
+q1 <- rnorm(n, sd=q)
+p <- rbinom(n, 2, 0.5) / 2
+q2 <- rep(NA, n)
+q2[p==0] <- rnorm(sum(p==0), sd=q)
+q2[p==0.5] <- rnorm(sum(p==0.5), mean=q1[p==0.5] / 2, sd = sqrt(0.75 * q^2))
+q2[p==1] <- q1[p==1]
+
+Y1 <- scale(q1 + F + rnorm(n, sd=r))
+Y2 <- scale(q2 + F + rnorm(n, sd=r))
+
+
+s2 <- (Y1 + Y2)^2
+d2 <- (Y1 - Y2)^2
+
