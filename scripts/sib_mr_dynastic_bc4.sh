d90b1c44b0d42684de304c111f143114e22bb206
Gibran Hemani <explodecomputer@gmail.com>
2018-08-22 23:44:36 +0100

    running updated dynastic analysis

---
+++ scripts/sib_mr_dynastic_bc4.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+#SBATCH --job-name=dynastic
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-200
+#SBATCH --output=job_reports/slurm-%A_%a.out
+#SBATCH --partition=mrcieu
+
+echo "Running on ${HOSTNAME}"
+module add R/3.2.3-foss-2016a
+
+if [ -n "${1}" ]; then
+  echo "${1}"
+  SLURM_ARRAY_TASK_ID=${1}
+fi
+
+i=${SLURM_ARRAY_TASK_ID}
+
+splits=200
+set -e
+
+mkdir -p ../results/sib_dynastic/
+Rscript \
+	sib_mr_dynastic.r \
+	${i} \
+	${splits} \
+	../results/sib_dynastic/sib_dynastic_${i}.rdata
