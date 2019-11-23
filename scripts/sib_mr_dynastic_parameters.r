94823def01ec3303243a03b39320280b762979c7
Gibran Hemani <explodecomputer@gmail.com>
2019-11-23 10:00:10 +0000

    updated sims

--- scripts/sib_mr_dynastic_parameters.r
+++ scripts/sib_mr_dynastic_parameters.r
@@ -3,13 +3,13 @@ main <- function(seed=1, output)
 	set.seed(seed)
 	param <- expand.grid(
 		n = c(10000, 20000, 40000, 60000, 100000),
-		nsim = 1:200,
 		nsnp = 90,
-		gen = c("ibd", "ibs", "ibs_unw"),
+		gen = c("ibs_unw"),
 		vargx = c(0.05, 0.1),
 		eff_xy = c(0, 0.001, 0.002, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05),
 		eff_ux = c(0, 0.1, 0.2),
-		eff_xu = c(0, 0.1, 0.2)
+		eff_xu = c(0, 0.1, 0.2),
+		nsim = 1:1000
 	)
 	param$gen <- as.character(param$gen)
 	write.table(param, file=output)
